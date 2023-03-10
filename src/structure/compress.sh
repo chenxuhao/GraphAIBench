5f0cc22348793d92da91cc7721357906882483f9
Xuhao Chen <cxh@mit.edu>
2023-03-10 08:51:59 -0500

    add compress.sh

---
+++ src/structure/compress.sh
@@ -0,0 +1,4 @@
+
+#../../bin/test_cgr_compressor ~/datasets/automine/citeseer/dag ~/datasets/automine/citeseer/dag-cgr
+../../bin/vbyte_compressor -s streamvbyte ~/datasets/automine/livej/dag ~/datasets/automine/livej/dag-streamvbyte
+
