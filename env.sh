dc7d8457cdf34409499d0c47436b2bf41251c475
Miranda J Cai <mjcai@satori-login-001.mit.edu>
2023-10-28 17:01:40 -0400

    edit env.sh for gpu

--- env.sh
+++ env.sh
@@ -1,12 +1,12 @@
 #export LD_LIBRARY_PATH=/usr/local/OpenBLAS/build/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/usr/local/cuda-11.3/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export LD_LIBRARY_PATH=/software/cuda/11.4/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 export KMP_AFFINITY=scatter
 export KMP_LIBRARY=turnaround
 export KMP_BLOCKTIME=0
 export OMP_NUM_THREADS=32
 
-export GPU_ARCH=80
-export CUDA_HOME=/usr/local/cuda
+export GPU_ARCH=70
+export CUDA_HOME=/software/cuda/11.4
 export NVSHMEM_USE_GDRCOPY=0
 export NVSHMEM_MPI_SUPPORT=1
 #export NVSHMEM_SHMEM_SUPPORT=1
@@ -19,8 +19,8 @@ export OPENBLAS_DIR=/usr/local/openblas
 export MKL_DIR=/opt/apps/sysnet/intel/20.0/mkl
 #MKLROOT = /opt/intel/mkl
 
-export GCC_HOME=/usr/lib/gcc/x86_64-linux-gnu/8
+export GCC_HOME=/usr/bin/gcc
 export CILK_HOME=/home/cxh/OpenCilk/build
 export CILK_CLANG=/home/cxh/OpenCilk/build/lib/clang/14.0.6
 
-export DATASET_PATH=/home/adrian/Documents/git/GraphAIBench/inputs/
+export DATASET_PATH=/home/cxh/datasets/automine/
