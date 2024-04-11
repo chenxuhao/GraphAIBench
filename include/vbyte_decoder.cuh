70af2e94afcfe34550fe0f70f6d60c0685225367
Miranda Cai <mcai1@gpua074.delta.ncsa.illinois.edu>
2024-04-11 16:41:39 -0500

    tests for any n decode

--- include/vbyte_decoder.cuh
+++ include/vbyte_decoder.cuh
@@ -68,6 +68,7 @@ __device__ void decode_streamvbyte_warp(uint32_t count, const uint32_t *in, uint
     //if (warp_lane == 0) printf("thread %d in warp %d has %d bytes, offset=%d, total_bytes=%d\n", thread_lane, warp_lane, num_bytes, offset, total_bytes);
     // Extract elements
     uint32_t val = (i < count) ? extract_bytes(&dataPtr[offset], num_bytes) : 0;
+    // printf("o %d; t %d; n %d\n", offset, total_bytes, num_bytes);
     dataPtr += total_bytes;
     if constexpr (delta) {
       uint32_t delta_val = 0;
@@ -75,15 +76,56 @@ __device__ void decode_streamvbyte_warp(uint32_t count, const uint32_t *in, uint
       // Compute prefix sum for differential/delta coding
       WarpScan(temp_storage[warp_lane]).InclusiveSum(val, delta_val);
       out[i] = delta_val;
-      // base = __shfl_sync(FULL_MASK, delta_val, WARP_SIZE-1);
+      base = __shfl_sync(FULL_MASK, delta_val, WARP_SIZE-1);
     } else {
       if (i < count) out[i] = val;
     }
   }
 }
 
+// template <bool delta = true>
+// __device__ void decode_streamvbyte_warp_thread(const uint32_t *in, uint32_t *out, int num) {
+//   int32_t count = *in;
+//   if (num < WARP_SIZE) { ++in; }
+//   if (count == 0) return;
+//   uint8_t *keyPtr = (uint8_t *)in; // full list of keys is next
+//   uint32_t keyLen = ((count + 3) / 4); // 2-bits per key (rounded up)
+//   uint8_t *dataPtr = keyPtr + keyLen;  // data starts at end of keys
+
+//   int thread_lane = threadIdx.x & (WARP_SIZE-1); // thread index within the warp
+//   int warp_lane   = threadIdx.x / WARP_SIZE;     // warp index within the CTA
+//   typedef cub::WarpScan<uint32_t> WarpScan;
+//   __shared__ typename WarpScan::TempStorage temp_storage[WARPS_PER_BLOCK];
+//   uint32_t base = 0;
+
+//   // Read the header
+//   uint32_t num_bytes = (thread_lane < count) ? (extract_bits(keyPtr, thread_lane * 2, 2) + 1) : 0;
+//   keyPtr += 8; // move 8 bytes forward; 8*8 = 64 = 32*2
+//   // Compute prefix sum to get the positions for extracting data elements
+//   uint32_t offset = 0;
+//   uint32_t total_bytes = 0;
+//   WarpScan(temp_storage[warp_lane]).ExclusiveSum(num_bytes, offset, total_bytes);
+//   // Extract elements
+//   uint32_t val = (thread_lane < count) ? extract_bytes(&dataPtr[offset], num_bytes) : 0;
+//   dataPtr += total_bytes;
+//   uint32_t delta_val = 0;
+//   if (thread_lane == 0) val += base;
+//   // Compute prefix sum for differential/delta coding
+//   WarpScan(temp_storage[warp_lane]).InclusiveSum(val, delta_val);
+//   out[thread_lane] = delta_val;
+//   base = __shfl_sync(FULL_MASK, delta_val, WARP_SIZE-1);
+// }
+
+// template <bool delta = true>
+// __device__ vidType decode_streamvbyte_warp1(const uint32_t *in, uint32_t *out) {
+//   uint32_t count = *in; // number of elements to decompress
+//   ++in;
+//   decode_streamvbyte_warp<delta>(count, in, out);
+//   return vidType(count);
+// }
+
 template <bool delta = true>
-__device__ void decode_streamvbyte_warp_thread(const uint32_t *in, uint32_t *out, int num) {
+__device__ void decode_streamvbyte_warp_thread(const uint32_t *in, uint32_t *out, int num, int n) {
   int32_t count = *in;
   if (num < WARP_SIZE) { ++in; }
   if (count == 0) return;
@@ -96,23 +138,18 @@ __device__ void decode_streamvbyte_warp_thread(const uint32_t *in, uint32_t *out
   typedef cub::WarpScan<uint32_t> WarpScan;
   __shared__ typename WarpScan::TempStorage temp_storage[WARPS_PER_BLOCK];
   uint32_t base = 0;
+  vidType sum = 0;
 
-  // Read the header
-  uint32_t num_bytes = (thread_lane < count) ? (extract_bits(keyPtr, thread_lane * 2, 2) + 1) : 0;
-  keyPtr += 8; // move 8 bytes forward; 8*8 = 64 = 32*2
-  // Compute prefix sum to get the positions for extracting data elements
-  uint32_t offset = 0;
-  uint32_t total_bytes = 0;
-  WarpScan(temp_storage[warp_lane]).ExclusiveSum(num_bytes, offset, total_bytes);
-  // Extract elements
-  uint32_t val = (thread_lane < count) ? extract_bytes(&dataPtr[offset], num_bytes) : 0;
-  dataPtr += total_bytes;
-  uint32_t delta_val = 0;
-  if (thread_lane == 0) val += base;
-  // Compute prefix sum for differential/delta coding
-  WarpScan(temp_storage[warp_lane]).InclusiveSum(val, delta_val);
-  out[thread_lane] = delta_val;
-  base = __shfl_sync(FULL_MASK, delta_val, WARP_SIZE-1);
+  for (int i = 0; i < n + 1; i ++) {
+    // Read the header
+    uint32_t num_bytes = extract_bits(keyPtr, i * 2, 2) + 1;
+    // keyPtr += 8; // move 8 bytes forward; 8*8 = 64 = 32*2
+    // Compute prefix sum to get the positions for extracting data elements
+    uint32_t offset = i;
+    uint32_t val = extract_bytes(&dataPtr[offset], num_bytes);
+    sum += val;
+  }
+  out[thread_lane] = sum;
 }
 
 template <bool delta = true>
--- src/compressing/test_decode.cu
+++ src/compressing/test_decode.cu
@@ -11,54 +11,77 @@ using namespace cooperative_groups;
 
 template <int scheme = 0, bool delta = true, int pack_size = 4>
 __global__ void test_warp_decompress(GraphGPUCompressed g, int total_threads, vidType *buffer) {
-    int thread_id = threadIdx.x + blockIdx.x * blockDim.x;
-    int warp_id = thread_id / WARP_SIZE;
-    if (thread_id >= total_threads) {
-        return;
-    }
-    vidType v_id = (vidType) warp_id;
-    vidType deg = g.get_degree(v_id);
-    vidType *adj = buffer + (g.get_max_degree() * v_id);
-    if (threadIdx.x % WARP_SIZE < deg) {
-        g.decode_vbyte_warp<scheme,delta,pack_size>(v_id, adj);
-    }
+  int thread_id = threadIdx.x + blockIdx.x * blockDim.x;
+  int warp_id = thread_id / WARP_SIZE;
+  if (thread_id >= total_threads) {
+    return;
+  }
+  // vidType v_id = (vidType) warp_id;
+  vidType v_id = 1;
+  vidType deg = g.get_degree(v_id);
+  vidType *adj = buffer + (g.get_max_degree() * v_id);
+  // if (threadIdx.x % WARP_SIZE < 1) {
+  //   g.decode_vbyte_warp_thread<scheme,delta,pack_size>(v_id, adj, 0, 1);
+  // }
+  if (threadIdx.x % WARP_SIZE < deg) {
+    g.decode_vbyte_warp<scheme,delta,pack_size>(v_id, adj);
+  }
 }
 
-__global__ void print_buffer(GraphGPUCompressed g, vidType *buffer) {
-    int thread_id = threadIdx.x + blockIdx.x * blockDim.x;
-    if (thread_id >= 1) {
-        return;
+__global__ void print_buffer(GraphGPUCompressed g, vidType *buffer, int num) {
+  int thread_id = threadIdx.x + blockIdx.x * blockDim.x;
+  if (thread_id >= 1) {
+    return;
+  }
+  for (int i = 0; i < num; i++) {
+    i = 1;
+    printf("v_id %d; deg %d\n", i, g.get_degree(i));
+    vidType *adj = buffer + (g.get_max_degree() * i);
+    // printf("v_id %d; adj %d %d %d %d\n", i, adj[0], adj[1], adj[2], adj[3]);
+    // printf("v_id %d; deg %d, adj[0] %d\n", i, g.get_degree(i), adj[0]);
+    for (int j = 0; j < g.get_degree(i); j++) {
+      printf("n_id%d: %d ", j, adj[j]);
     }
-    for (int i = 0; i < g.V(); i++) {
-        vidType *adj = buffer + (g.get_max_degree() * i);
-        printf("v_id %d; adj %d %d %d %d\n", i, adj[0], adj[1], adj[2], adj[3]);
-    }
+  }
 }
 
 void move_onto_gpu(Graph &g) {
-    GraphGPUCompressed gg(g, "streamvbyte", g.get_degree_threshold(), 0, 1, true);
-    vidType *buffer;
-    vidType max_degree = g.get_max_degree();
-    int block_size = 256;
-    size_t warps_per_block = block_size / WARP_SIZE;
-    size_t nblocks = (g.V()-1)/warps_per_block+1;
-    allocate_gpu_buffer(size_t(max_degree) * warps_per_block * nblocks, buffer);
-    int threads = g.V() * WARP_SIZE;
-    int num_blocks = (threads + block_size - 1) / block_size;
-    test_warp_decompress<<<num_blocks,block_size>>>(gg, threads, buffer);
-    cudaDeviceSynchronize();
-    print_buffer<<<1,1>>>(gg, buffer);
-    cudaDeviceSynchronize();
-    cudaFree(buffer);
+  GraphGPUCompressed gg(g, "streamvbyte", g.get_degree_threshold(), 0, 1, true);
+  vidType *buffer;
+  vidType max_degree = g.get_max_degree();
+  // int num = g.V();
+  int num = 1;
+  int block_size = 256;
+  size_t warps_per_block = block_size / WARP_SIZE;
+  size_t nblocks = (num-1)/warps_per_block+1;
+  allocate_gpu_buffer(size_t(max_degree) * warps_per_block * nblocks, buffer);
+  int threads = num * WARP_SIZE;
+  int num_blocks = (threads + block_size - 1) / block_size;
+  test_warp_decompress<<<num_blocks,block_size>>>(gg, threads, buffer);
+  cudaDeviceSynchronize();
+  print_buffer<<<1,1>>>(gg, buffer, num);
+  cudaDeviceSynchronize();
+  cudaFree(buffer);
 }
 
 int main(int argc, char* argv[]) {
-    Graph g;
-    std::string in_prefix = argv[1];
-    std::string out_prefix = argv[2];
-    std::string scheme = "streamvbyte";
-    bool permutated = false;
-    // save_compressed_graph(in_prefix, out_prefix);
-    g.load_compressed_graph(out_prefix, scheme, permutated);
-    move_onto_gpu(g);
+  Graph g;
+  std::string in_prefix = argv[1];
+  std::string out_prefix = argv[2];
+  std::string scheme = "streamvbyte";
+  bool permutated = false;
+  bool compress_graph = false;
+  int c;
+  while ((c = getopt(argc, argv, "c")) != -1) {
+    switch (c) {
+      case 'c':
+      compress_graph = true;
+      break;
+      default:
+      abort();
+    }
+  }
+  if (compress_graph) { save_compressed_graph(in_prefix, out_prefix); }
+  g.load_compressed_graph(out_prefix, scheme, permutated);
+  move_onto_gpu(g);
 }
