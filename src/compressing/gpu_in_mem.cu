686791e6e40ed633e8798d557d3ef1f905b8b3fa
Miranda Cai <mcai1@gpua005.delta.ncsa.illinois.edu>
2024-04-25 14:53:25 -0500

    add faster sample parallel method

--- src/compressing/gpu_in_mem.cu
+++ src/compressing/gpu_in_mem.cu
@@ -12,7 +12,7 @@ using namespace cooperative_groups;
 // const int BLOCK_DIM = 32;
 // const vidType MAX_VIDTYPE = 0 - 1;
 
-__global__ void khop_next_warp(GraphGPU g, int total_threads, int n_steps, int *step_counts, vidType *result, curandState *states) {
+__global__ void khop_next_warp(GraphGPU g, int total_threads, int n_steps, int n_samples, int *step_counts, vidType *result, curandState *states) {
     int thread_id = threadIdx.x + blockIdx.x * blockDim.x;
     int warp_id = thread_id / WARP_SIZE;
     if (thread_id >= total_threads) {
@@ -20,7 +20,7 @@ __global__ void khop_next_warp(GraphGPU g, int total_threads, int n_steps, int *
     }
     curandState local_state = states[thread_id];
     
-    int step_count = step_counts[0];
+    int step_count = n_samples * step_counts[0];
     int t_begin = step_count;
     int old_t_begin = 0;
     grid_group grid = this_grid();
@@ -50,14 +50,14 @@ __global__ void khop_next_warp(GraphGPU g, int total_threads, int n_steps, int *
     }
 }
 
-__global__ void khop_next(GraphGPU g, int total_threads, int n_steps, int *step_counts, vidType *result, curandState *states) {
+__global__ void khop_next_tp(GraphGPU g, int total_threads, int n_steps, int n_samples, int *step_counts, vidType *result, curandState *states) {
     int thread_id = threadIdx.x + blockIdx.x * blockDim.x;
     if (thread_id >= total_threads) {
         return;
     }
     curandState local_state = states[thread_id];
     
-    int step_count = step_counts[0];
+    int step_count = n_samples * step_counts[0];
     int t_begin = step_count;
     int old_t_begin = 0;
     grid_group grid = this_grid();
@@ -82,6 +82,36 @@ __global__ void khop_next(GraphGPU g, int total_threads, int n_steps, int *step_
     }
 }
 
+__global__ void khop_next_sp(GraphGPU g, int total_threads, int n_steps, int n_samples, int *step_counts, vidType *result, curandState *states) {
+    int thread_id = threadIdx.x + blockIdx.x * blockDim.x;
+    if (thread_id >= total_threads) {
+        return;
+    }
+    curandState local_state = states[thread_id];
+    
+    int step_count = n_samples;
+    int t_begin = step_count;
+    int old_t_begin = 0;
+    for (int step = 0; step < n_steps; step++) {
+      int prev_step_sample_size = step_counts[step];
+      int step_sample_size = step_counts[step + 1];
+      for (int i = 0; i < step_sample_size; i++) {
+        int old_t_idx = old_t_begin + (thread_id * prev_step_sample_size) + (i / step_sample_size);
+        vidType old_t = result[old_t_idx];
+        int t_idx = t_begin + (thread_id * step_sample_size) + i;
+        if (old_t == MAX_VIDTYPE) {
+          result[t_idx] = MAX_VIDTYPE;
+        } else {
+          vidType old_t_deg = g.get_degree(old_t);
+          result[t_idx] = get_next_gpu(g, old_t, old_t_deg, local_state);
+        }
+      }
+      step_count *= step_sample_size;
+      old_t_begin = t_begin;
+      t_begin += step_count;
+    }
+}
+
 
 double multilayer_sample(Graph &g, vector<vidType>& initial, int n_samples, int total_num, int last_step_num, vidType* result, int block_size, bool use_uva) {
     GraphGPU gg(g, use_uva);
@@ -97,7 +127,6 @@ double multilayer_sample(Graph &g, vector<vidType>& initial, int n_samples, int 
         result[i] = initial[i];
     }
     sizes_list(n_steps, step_counts);
-    step_counts[0] *= n_samples;
     alloc_t = seconds();
     CUDA_SAFE_CALL(cudaMalloc((void **)&d_result, total_num * size));
     CUDA_SAFE_CALL(cudaMemcpy(d_result, result, cur_num * size, cudaMemcpyHostToDevice));
@@ -109,7 +138,8 @@ double multilayer_sample(Graph &g, vector<vidType>& initial, int n_samples, int 
 
     int max_threads = 40000;
     int threads_needed = last_step_num / sample_size(steps() - 1);
-    int total_threads = min(threads_needed * WARP_SIZE, max_threads);
+    // int total_threads = min(threads_needed * WARP_SIZE, max_threads);
+    int total_threads = max_threads;
     int num_blocks = (total_threads + block_size - 1) / block_size;
     rand_t = seconds();
     setup_kernel<<<num_blocks,block_size>>>(d_states);
@@ -119,13 +149,15 @@ double multilayer_sample(Graph &g, vector<vidType>& initial, int n_samples, int 
     std::cout << "Starting sampling with " << total_threads << " threads...\n";
     dim3 block(block_size, 1, 1);
     dim3 grid(num_blocks, 1, 1);
-    void *kernel_args[] = {&gg, &total_threads, &n_steps, &d_step_counts, &d_result, &d_states};
+    void *kernel_args[] = {&gg, &total_threads, &n_steps, &n_samples, &d_step_counts, &d_result, &d_states};
     sample_t = seconds();
-    if (total_threads == max_threads) {
-      cudaLaunchCooperativeKernel((void*)(khop_next), grid, block, kernel_args);
-    } else {
-      cudaLaunchCooperativeKernel((void*)(khop_next_warp), grid, block, kernel_args);
-    }
+    // if (total_threads == max_threads) {
+    //   cudaLaunchCooperativeKernel((void*)(khop_next_sp), grid, block, kernel_args);
+    // } else {
+    //   cudaLaunchCooperativeKernel((void*)(khop_next_warp), grid, block, kernel_args);
+    // }
+    // cudaLaunchCooperativeKernel((void*)(khop_next_sp), grid, block, kernel_args);
+    khop_next_sp<<<num_blocks,block_size>>>(gg, total_threads, n_steps, n_samples, d_step_counts, d_result, d_states);
     CUDA_SAFE_CALL(cudaDeviceSynchronize());
     sample_t = seconds() - sample_t;
 
